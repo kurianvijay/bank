0e09710ca56231ef10446b195f5127b555c14a5e
Vijay Kurian <kurianvijay7@gmail.com>
2019-11-19 11:56:00 +0000

    Add, private methods for date and amount formatting, transaction_history in account initialization, which records each transaction, tests passing

--- lib/account.rb
+++ lib/account.rb
@@ -1,22 +1,41 @@
 # frozen_string_literal: true
 
 class Account
-  attr_reader :balance
+  attr_reader :balance, :transaction_history
 
-  def initialize(name)
-    @name = name
+  def initialize
     @balance = 0
+    @transaction_history = []
   end
 
   def deposit(amount)
     @balance += amount
+    @transaction_history << {date: date_formatter, amount: amount_formatter(amount), balance: @balance}
+    @balance
   end
 
   def withdraw(amount)
     if amount > @balance
       raise "Cannot withraw more than avaliable balance #{@balance}"
       end
-
     @balance -= amount
+    @transaction_history << {date: date_formatter, amount: amount_formatter(amount), balance: @balance}
+    @balance
   end
+
+private
+
+  def date_formatter
+    Time.now.strftime("%d/%m/%Y")
+  end
+
+  def amount_formatter(a)
+    '%.2f' % a
+  end
+
 end
+
+# date || credit || debit || balance
+# 14/01/2012 || || 500.00 || 2500.00
+# 13/01/2012 || 2000.00 || || 3000.00
+# 10/01/2012 || 1000.00 || || 1000.00
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,12 +3,13 @@
 require 'account'
 
 describe Account do
-  subject(:account) { described_class.new('name') }
+  subject(:account) { described_class.new }
   let(:balance) { account.balance }
 
-  describe 'balance' do
+  describe '#new' do
     it 'allows a user to view balance in their account' do
       expect(account.balance).to eq 0
+      expect(account.transaction_history).to eq []
     end
   end
 
@@ -29,4 +30,10 @@ describe Account do
       expect { account.withdraw(11) }.to raise_error("Cannot withraw more than avaliable balance #{balance}")
     end
   end
+
 end
+
+# date || credit || debit || balance
+# 14/01/2012 || || 500.00 || 2500.00
+# 13/01/2012 || 2000.00 || || 3000.00
+# 10/01/2012 || 1000.00 || || 1000.00
